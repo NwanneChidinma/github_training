86a09ee18f7449b20677a35fc9cf877fc5b09936
Dennis Isong <drexpert.info@gmail.com>
2026-04-09 20:56:16 -0700

    my test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 print("Hello from Session 2")
-<<<<<<< HEAD
 print("Hello from Student A") 
 print("Hello from Student B")
 
 print("I'm doing great, thank you!")
 print("I'm doing great, thank you!")
-=======
->>>>>>> parent of 4304026 (feat: update greeting from Student B)
+print("Hello from Student A")
+print("I'm doing great, thank you!")
+print("I'm doing great, thank you!")
 print("Hello from Student A")
