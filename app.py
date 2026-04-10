9458c79d0e2d44075769462d08f02ec994e9ba0d
DENNIS ISONG <dennisisong@DENNISs-MacBook-Pro.local>
2026-04-09 20:26:57 -0700

    merge restore

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 print("Hello from Session 2")
+<<<<<<< HEAD
 print("Hello from Student A") 
 print("Hello from Student B")
 print("How are you doing today?")
 
 print("I'm doing great, thank you!")
 print("I'm doing great, thank you!")
+=======
+>>>>>>> parent of 4304026 (feat: update greeting from Student B)
 print("Hello from Student A")
